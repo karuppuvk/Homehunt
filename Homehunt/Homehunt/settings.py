d67856b2c905628dee7f36078fddef2eb130dcd8
karuppuvk <skaruppu869@gmail.com>
2026-02-03 17:15:29 +0530

    model and index updated too

--- Homehunt/Homehunt/settings.py
+++ Homehunt/Homehunt/settings.py
@@ -66,6 +66,9 @@ TEMPLATES = [
         },
     },
 ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 WSGI_APPLICATION = "Homehunt.wsgi.application"
 
@@ -115,4 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
